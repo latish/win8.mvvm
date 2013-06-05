d94ad7bf27c7dff3e3affae5a88a7adbff1d5aae
Latish Sehgal <latish.sehgal@gmail.com>
2013-06-05 11:22:57 -0500

    Removing demo code

--- Win8.Mvvm/ViewModel/MainViewModel.cs
+++ Win8.Mvvm/ViewModel/MainViewModel.cs
@@ -6,39 +6,13 @@ using Win8.Mvvm.Services;
 namespace Win8.Mvvm.ViewModel
 {
 
-	public class MainViewModel : ViewModelBase
+	public class MainViewModel
 	{
 		private readonly IDataService _dataService;
-		private RelayCommand _refreshCommand;
 
 		public MainViewModel(IDataService dataService)
 		{
 			_dataService = dataService;
-			Items = new ObservableCollection<ScheduleItemViewModel>();
-
-		}
-
-		public ObservableCollection<ScheduleItemViewModel> Items { get; private set; }
-
-		public RelayCommand RefreshCommand
-		{
-			get
-			{
-				return _refreshCommand
-					 ?? (_refreshCommand = new RelayCommand(ExecuteRefreshCommand));
-			}
-		}
-
-		private void ExecuteRefreshCommand()
-		{
-			var items = _dataService.GetSchedule();
-
-			if (items != null)
-			{
-				Items.Clear();
-				foreach (var scheduleItem in items)
-					Items.Add(new ScheduleItemViewModel(scheduleItem));
-			}
 		}
 	}
 }
